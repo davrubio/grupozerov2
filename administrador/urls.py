80361be932698dc7237d9d5a36fe967cb0c4b21d
David Rubio <dav.rubio@duocuc.cl>
2023-07-03 20:30:40 -0400

    update

--- administrador/urls.py
+++ administrador/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('menu', views.menu, name='menu'),
-    path('home', views.home, name='home'),
+    path('salir/', views.salir, name='salir'),
+
 ]
--- administrador/views.py
+++ administrador/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 # Create your views here.
+@login_required
 def menu(request):
-    request.session["usuario"]="David"
-    usuario = request.session["usuario"]
-    context = {'usuario':usuario}
-    return render(request, 'administrador/menu.html', context)
+    #request.session["usuario"]="David"
+    #usuario = request.session["usuario"]
+    #context = {'usuario':usuario}
+    return render(request, 'administrador/menu.html')
 
-def home(request):
-    context = {}
-    return render(request, 'administrador/home.html', context)
+def salir(request):
+    logout(request)
+    return redirect("index")
 
 #def posters(request):
     #traer lista de posters ingresados
