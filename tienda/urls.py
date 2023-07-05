42965fa970de37a3495064e30d0b2e2f8b04b02c
Jorge <jescobar@subcargo.com>
2023-07-05 01:08:09 -0400

    últimos cambios

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,9 +3,13 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
+    path('carro', views.carro, name='carro'),
     path('galeria', views.galeria, name='galeria'),
     path('contacto', views.contacto, name='contacto'),
     path('nosotros', views.nosotros, name='nosotros'),
     path('cuadrosAdd', views.cuadrosAdd, name='cuadrosAdd'),
-    path('poster_customer', views.poke, name='poster_customer'),
+    path('poster_customer', views.poke,  name='poster_customer'),
+    path('cuadrosEdit/<str:id>', views.cuadrosEdit, name='cuadrosEdit'),
+    path('cuadrosDelete/<str:id_cuadro>', views.cuadrosDelete,  name='cuadrosDelete'),
+    path('posterAdd/<str:nombre>/<str:foto>/<str:number>', views.posterAdd,  name='posterAdd'),
 ]
